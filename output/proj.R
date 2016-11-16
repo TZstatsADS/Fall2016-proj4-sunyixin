c98407c04fa4626f3267d5fdb4d8a567e1035005
sunyixin <ys2879@columbia.edu>
2016-11-16 11:46:55 +0800

    Add files via upload

---
+++ output/proj.R
@@ -0,0 +1,96 @@
+library(rhdf5)
+files.list = list.files("data", full.names = T, recursive = T)
+n = length(files.list) #2350
+features = NULL
+for(i in 1:n) 
+{
+  sound = h5read(files.list[i], "/analysis")
+  songs = sound$songs #songs attribute
+  sound$songs = NULL
+  sound$sections_confidence = NULL
+  sound$sections_start = NULL
+  #Basic statistics for other attributes, including mean, median and standard deviation
+  basic_stats = lapply(sound, function(x) {
+    m1 = mean(x)
+    m2 = median(x)
+    s = sd(x)
+    c(m1,m2,s)
+  })
+  basic_stats_vec = unlist(basic_stats) 
+  #use tempo and loudness of songs
+  songs_tempo = songs$tempo
+  songs_loudness = songs$loudness
+  features = rbind(features,
+                   c(songs_tempo, songs_loudness, basic_stats_vec)) 
+  H5close()
+}
+#song names
+song = substr(files.list, 12, nchar(files.list)-3)
+rownames(features) = song
+colnames(features)[1:2] = c("tempo","loudness")
+dim(features)
+View(features) 
+#save the features
+save(features, file = "proj_features.Rdata")
+load("proj_features.Rdata")
+
+
+files.list = list.files("data", full.names = T, recursive = T)
+n = length(files.list[1:100]) #100 test songs
+features2 = NULL
+for(i in 1:n) 
+{
+  sound = h5read(files.list[i], "/analysis")
+  songs = sound$songs #songs attribute
+  sound$songs = NULL
+  sound$sections_confidence = NULL
+  sound$sections_start = NULL
+  #Basic statistics for other attributes, including mean, median and standard deviation
+  basic_stats = lapply(sound, function(x) {
+    m1 = mean(x)
+    m2 = median(x)
+    s = sd(x)
+    c(m1,m2,s)
+  })
+  basic_stats_vec = unlist(basic_stats) 
+  #use tempo and loudness of songs
+  songs_tempo = songs$tempo
+  songs_loudness = songs$loudness
+  features2 = rbind(features2,
+                   c(songs_tempo, songs_loudness, basic_stats_vec)) 
+  H5close()
+}
+
+load("lyr.RData")
+
+features_all = rbind(features2, features)
+features_all = scale(features_all)
+distances = dist(features_all)
+
+distances = as.matrix(distances)
+
+
+knn = apply(distances[101:2400,1:100],2,function(x) order(x)[1:20])
+
+rec = apply(knn,2, function(x) {
+  apply(lyr[x,-1],2,sum)
+})
+
+song = substr(files.list[1:100], 12, nchar(files.list)-3)
+colnames(rec) = song
+View(rec)
+words = row.names(rec)
+best.word = words[apply(rec,2,which.max)]
+best.word
+#Most possible word has lower rank
+rank.word = apply(rec,2,function(x) nrow(rec)+ 1 - order(x))
+View(rank.word)
+
+
+
+
+
+
+
+
+
